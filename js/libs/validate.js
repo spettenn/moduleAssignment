435c722eaaff1f651ade0933527017c33c49620d
spettenn <69841778+spettenn@users.noreply.github.com>
2021-10-17 17:34:10 +0200

    last push for module assignment

--- js/libs/validate.js
+++ js/libs/validate.js
@@ -9,9 +9,16 @@ export const testLengthofTextBoxValue = function (
 	}
 };
 
+export const testUsername = function (username) {
+	if (username.length <= 3) {
+		return false;
+	} else {
+		return true;
+	}
+};
+
 export const testEmailAddress = function (emailAddress) {
 	const regexString =
 		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	// if it passes will return true if it fails will return false
 	return regexString.test(emailAddress);
 };
